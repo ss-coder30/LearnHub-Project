3650e02ee4367831e00255b8a3a2132e002da094
Shaurya Srivastava <30shaurya@gmail.com>
2024-07-30 17:39:07 +0530

    completed course completion page

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,12 +13,12 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-red-200 hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        success: "bg-green-500 text-white hover:bg-green-600",
+        success: "bg-green-600 text-white hover:bg-emarald-600/80",
       },
       size: {
         default: "h-10 px-4 py-2",
